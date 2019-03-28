34e557b4c46df477da326ac23c327cd8fa3306e5
Mohammad Hossain <mohammadthossain@gmail.com>
2019-03-28 02:35:13 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+
+def greeting(name)
+  return "Hi, my name is #{name}." 
+end
+
+#puts greeting("Henry")
+
+def programming(name,language)
+  return greeting#(name) + "and I am learning to program in" + #{language}
+end
+
+
+puts programming("Henry"),("Ruby")
